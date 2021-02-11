19b58ce3fcbba3c338a1f744959fb97fd6b7b7f4
Nicolás Tobo <na.tobo@uniandes.edu.co>
2021-02-11 10:21:44 -0500

    privacidad

--- public/env.js
+++
@@ -1,6 +0,0 @@
-window.env = {
-  "USER_POOL_ID": "us-east-1_vRp4t77EF",
-  "CLIENT_ID": "4i6ajc9qfbimagvftp3hcg4b6a",
-  "API_ENDPOINT_IMG": "https://9ypkxvdgb7.execute-api.us-east-1.amazonaws.com/Prod/sign-s3",
-  "API_ENDPOINT_CRUD": "https://9ypkxvdgb7.execute-api.us-east-1.amazonaws.com/Prod/Lambda_api-lambda-db-tiendaback-nicotobo"
-};
